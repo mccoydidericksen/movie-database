84ea8d1133b4279a095ff095e1b0ffe0628f2876
Jonathan Pohahau <j.pohahau@outlook.com>
2023-02-01 13:18:53 -0800

    added sql

---
+++ db/seeds.sql
@@ -0,0 +1,18 @@
+INSERT INTO movies (movie_name)
+VALUES ('Avatar: The Way of Water'),
+       ('M3gan'),
+       ('Plane'),
+       ('Missing'),
+       ('The Whale');
+
+INSERT INTO reviews (movie_id, review)
+VALUES (1, "I have seen so many movies and no one is perfect but I can say this movie is one of the best productions I have seen in years."),
+       (1, "An astonishing achievement in long-form visual effects, but not in storytelling."),
+       (2 "After seeing this in the theater, I definitely have some things to say about this horror flick including my thoughts."),
+       (2, "At the heart of this film is entertainment it knows what it is, it knows what its job is, and it does that flawlessly."),
+       (3, "PLANE is an almost perfect movie."),
+       (3, "Overall, it is an okay film that passes the time."),
+       (4, "Kept me on the edge of my seat. Good plot twists and what not."),
+       (4, "this movie is a 10/10 lots of twists and turns and a nice roller coaster ride they picked the right people to cast for the movie highly recommended im 14 btw"),
+       (5, "I don't usually write reviews, but I was compelled to do so for this film by what I can only call a life-changing experience."),
+       (5, "I joined a friend to see this movie on this very cold and snowy January day, and I am happy to report venturing out was an excellent choice! Brendan Fraser gives a poignant and Oscar quality performance as a grieving");
